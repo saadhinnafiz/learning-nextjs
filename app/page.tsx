396fbec6e4d9af92d6af92ea3678d761cf518ffb
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-16 15:42:31 +0500

    feat: add Hello component and explore server vs client components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import Hello from "./components/Hello";
+
 const Home = () => {
-  return <div>Welcome to Next.js!</div>;
+  console.log("What tyoe of component am I?");
+  return (
+    <main>
+      <div className="text-4xl underline">Welcome to Next.js!</div>
+      <Hello />
+    </main>
+  );
 };
 
 export default Home;
